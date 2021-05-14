df3873a09fda96dd4d93c130137c828af8dd0f23
Gabriel Domínguez Reyes <gdominguezreyes@danielcastelao.com>
2021-05-14 09:46:36 +0200

    Ejercicio 3 terminado

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,25 @@
 fun main(args: Array<String>) {
+    println("Tabla del 6")
     tabla6.forEach{
         println("6*${it/6} = ${it}")
+    }
+
+    println("Numeros Random")
+    arrayRan.forEach{
+        println("Numero= ${it}")
+    }
+
+    fun miembro(){
+
+
+
+    }
+
+    println("Impares")
+    arrayRan.forEach{
+        if(it%2 != 0)
+        println("Numero= ${it}")
+
     }
 
 
