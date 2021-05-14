a9e79fbc891dc97fc62e95c680864eda98bef48b
Gabriel Domínguez Reyes <gdominguezreyes@danielcastelao.com>
2021-05-14 10:10:29 +0200

    Tercer Ejercicio correcto

--- src/main/kotlin/claseB.kt
+++ src/main/kotlin/claseB.kt
@@ -1,8 +1,10 @@
-fun miembro(){
+class claseB {
+    fun miembro(Array:Int) {
 
-    arrayRan.filter{
-        arrayRan.size %2 != 0
-    }.forEach{
-        println(it)
+        arrayRan.filter {
+            arrayRan.size % 2 != 0
+        }.forEach {
+            println(it)
+        }
     }
 }
